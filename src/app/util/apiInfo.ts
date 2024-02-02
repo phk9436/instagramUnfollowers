31008fd0fb0d9bcece84339aacc7fc61a50c2332
hgpark <digitalworks_hgpark@amorepacific.com>
2024-02-02 14:31:09 +0900

    [Feat] accessCode 요청 기능 추가

---
+++ src/app/util/apiInfo.ts
@@ -0,0 +1,3 @@
+export const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+export const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+export const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
